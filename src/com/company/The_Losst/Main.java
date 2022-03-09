9addf34d35a61178c5b2edd8acd6c9e7815cc0ba
TheL0st <79166172444@yandex.ru>
2022-03-09 14:01:44 +0300

    Доработка базовых функций

--- src/com/company/The_Losst/Main.java
+++ src/com/company/The_Losst/Main.java
@@ -1,7 +1,7 @@
 package com.company.The_Losst;
 
 import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
+
 
 import java.io.*;
 import java.io.IOException;
@@ -14,27 +14,30 @@ public class Main {
         FileWriter writer = new FileWriter("C:\\Users\\TheLosst\\IdeaProjects\\Parser_Hippo\\src\\com\\company\\The_Losst\\out.txt");
         int global = 0;
         Gson g = new Gson();
-        String all_text = "";
+        StringBuilder all_text = new StringBuilder();
         int count = 0;
         try (FileReader reader = new FileReader("C:\\Users\\TheLosst\\IdeaProjects\\Parser_Hippo\\src\\com\\company\\The_Losst\\File.txt")) {
             int c;
             while ((c = reader.read()) != -1) {
-                all_text += (char) c;
+                all_text.append((char) c);
             }
             //System.out.println(all_text);
         }
-        all_text = all_text.replaceAll("\\n","");
-        all_text = all_text.replaceAll("\\r","");
+        all_text = new StringBuilder(all_text.toString().replaceAll("\\n", ""));
+        all_text = new StringBuilder(all_text.toString().replaceAll("\\r", ""));
         Scanner in = new Scanner(System.in);
         System.out.println("Введите количество лошадей в заезде :");
         int amount_of_horses = in.nextInt();
 
         while (global < amount_of_horses) {
-            String NOMER = "", klichka = "", mast = "", naezdnik = "";
-            boolean number_try = false, klichka_try = false, mast_try = false, naezdnik_try = false;
+            String NOMER = "";
+            StringBuilder klichka = new StringBuilder();
+            StringBuilder mast = new StringBuilder();
+            StringBuilder naezdnik = new StringBuilder();
+            boolean number_try = false, klichka_try = false, mast_try = false;
             while (!(number_try)) {
                 if ((all_text.charAt(count) == '.')) {
-                    NOMER = String.valueOf(all_text.charAt(count - 2)) + String.valueOf(all_text.charAt(count - 1)) + String.valueOf(all_text.charAt(count));
+                    NOMER = all_text.charAt(count - 2) + String.valueOf(all_text.charAt(count - 1)) + all_text.charAt(count);
                     number_try = true;
                     count_before = count + 1;
                 }
@@ -44,7 +47,7 @@ public class Main {
             while (!(klichka_try)) {
                 if ((String.valueOf(all_text.charAt(count + 1)).equals("-"))) {
                     for (int i = count_before; i < count + 1; i++) {
-                        klichka += String.valueOf(all_text.charAt(i));
+                        klichka.append(all_text.charAt(i));
 
                     }
                     klichka_try = true;
@@ -59,7 +62,7 @@ public class Main {
                 if (((all_text.charAt(count + 8) == 'к') & (all_text.charAt(count + 7) == 'и') & (all_text.charAt(count + 6) == 'н') & (all_text.charAt(count + 5) == 'д') & (all_text.charAt(count + 4) == 'з') & (all_text.charAt(count + 3) == 'е') & (all_text.charAt(count + 2) == 'а') & (all_text.charAt(count + 1) == 'Н')) | ((all_text.charAt(count + 4) == 'т') & (all_text.charAt(count + 3) == 'е') & (all_text.charAt(count + 2) == 'д') & (all_text.charAt(count + 1) == 'Е')) | ((all_text.charAt(count + 6) == 'р') & (all_text.charAt(count + 5) == 'е') & (all_text.charAt(count + 4) == 'т') & (all_text.charAt(count + 3) == 'с') & (all_text.charAt(count + 2) == 'а') & (all_text.charAt(count + 1) == 'М'))) {
                     mast_try = true;
                     for (int i = count_before; i < count + 1; i++) {
-                        mast += String.valueOf(all_text.charAt(i));
+                        mast.append(all_text.charAt(i));
                     }
                     count_before = count;
                 }
@@ -67,15 +70,12 @@ public class Main {
             }
             //System.out.println("Ready " + count);
 
-            if (!(naezdnik_try)) {
-                int resetID = all_text.indexOf(".", count + 40);
-                for (int i = count_before; i < resetID; i++) {
-                    naezdnik += String.valueOf(all_text.charAt(i));
-                    naezdnik_try = true;
-                }
-                count = resetID + 15;
-                naezdnik += ".";
+            int resetID = all_text.indexOf(".", count + 40);
+            for (int i = count_before; i < resetID; i++) {
+                naezdnik.append(all_text.charAt(i));
             }
+            count = resetID + 15;
+            naezdnik.append(".");
 
 
             //System.out.println("Стартовый номер " + NOMER);
@@ -84,7 +84,7 @@ public class Main {
             //System.out.println("Наездник " + naezdnik);
             global++;
             System.out.println("--------------------------------------------------------------------------");
-            Hippo_Parser part = new Hippo_Parser(NOMER,klichka,mast,naezdnik);
+            Hippo_Parser part = new Hippo_Parser(NOMER, klichka.toString(), mast.toString(), naezdnik.toString());
             Scanner input = new Scanner(System.in);
             System.out.println("Введите место на финише лошади с номером " + NOMER + " " + klichka + ":");
             String  buff = input.nextLine();
